fe7ba74569a154c5e5d8e585a76b0d321333ca65
Adel Vilkov <vilkov.adel@gmail.com>
2023-05-22 13:38:26 +0300

    Suppressed AOT warning, made AOT-specific attributes internal

--- Ignis/Attributes/DynamicallyAccessedMembersAttribute.cs
+++ Ignis/Attributes/DynamicallyAccessedMembersAttribute.cs
@@ -1,6 +1,6 @@
-#define SYSTEM_PRIVATE_CORELIB
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+#nullable enable
 
 namespace System.Diagnostics.CodeAnalysis
 {
--- Ignis/Attributes/RequiresDynamicCodeAttribute.cs
+++ Ignis/Attributes/RequiresDynamicCodeAttribute.cs
@@ -1,7 +1,6 @@
-#define SYSTEM_PRIVATE_CORELIB
-#nullable enable
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+#nullable enable
 
 namespace System.Diagnostics.CodeAnalysis
 {
